bd07634d6bbf66c873329230b7a6c91e6bf63104
kuldeep-rathor <kuldeeprathor91010@gmail.com>
2023-07-27 10:48:47 +0530

    HeaderAdded

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Layout/Header";
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assests/meals.jpg";
+import HaederCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HaederCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
